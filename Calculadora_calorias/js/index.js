c881722ffe781223c876489f5cd3c23da0940bfe
jcorona2019066 <losxman19@gmail.com>
2022-08-03 01:52:33 -0600

    RetoDeportivo

--- Calculadora_calorias/js/index.js
+++
@@ -1,8 +0,0 @@
-const botonesNumeros = document.querySelectorAll(".form-control");
-const selectorNumeros = document.querySelectorAll(".form-select");
-const displayValorActual = document.getElementById("valor-Actual");
-
-const cal = new Calculadora();
-
-/* console.log(cal.mujeres(30,132.3,170))
-console.log(cal.hombres(45,209.48,190)) */
