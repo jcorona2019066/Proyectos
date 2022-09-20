007fbdaffd60e1edbdd68af2c9eda5b82b3540d5
jcorona2019066 <69706146+jcorona2019066@users.noreply.github.com>
2022-09-20 01:09:17 -0600

    Delete Importadora directory

--- Importadora/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-//redirije al inicio de la pagina
-const express = require('express');
-const router = express.Router();
-
-router.get('/', async (req, res) => {
-    res.render('index');
-});
-
-module.exports = router;
