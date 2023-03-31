495d43550badd314d5148d1584d420796049e729
jcorona2019066 <losxman19@gmail.com>
2023-03-30 19:29:18 -0600

    prueba pagina web

---
+++ Pagina web/main.js
@@ -0,0 +1,7 @@
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
+ScrollReveal().reveal('.nav-main', { delay: 500 });
+ScrollReveal().reveal('.nav-menu-right', { delay: 500 });
+ScrollReveal().reveal('.contenedor3', { delay: 500 });
